fc6829c7429c81bb57bcca10e4fffd0103b8c152
Logan Boyd <logan.boyd@protonmail.com>
2023-09-09 17:04:01 -0400

    Add theta

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const f = parseFloat(param3.value);
         const w = parseFloat(param4.value);
         const phi = parseFloat(param5.value);
+        const theta = parseFloat(param6.value);
 
-        const pathString = generatePathFromParams(A, d, f, w, phi);
+        const pathString = generatePathFromParams(A, d, f, w, phi, theta);
 
         svg.innerHTML = `<path d="${pathString}" stroke="black" fill="none" stroke-width="1"/>`;
     }
@@ -20,13 +21,16 @@ document.addEventListener('DOMContentLoaded', function() {
     param3.addEventListener('input', updateSVG);
     param4.addEventListener('input', updateSVG);
     param5.addEventListener('input', updateSVG);
+    param6.addEventListener('input', updateSVG);
   
     updateSVG();
 });
 
 const N_SAMPLES = 500;
 
-function generatePathFromParams(A, d, f, w, phi) {
+function generatePathFromParams(A, d, f, w, phi, theta) {
+    console.log(theta);
+
     const samples = Array.from({ length: N_SAMPLES }, (_, r) => r)
         .map(r => 0 + (2 * Math.PI - 0) * r / (500 - 1));
 
@@ -34,8 +38,8 @@ function generatePathFromParams(A, d, f, w, phi) {
 
     let points = samples
         .map(r => ({
-            x: z(r) * Math.sin(r),
-            y: z(r) * Math.cos(r),
+            x: z(r) * Math.sin(r - theta),
+            y: z(r) * Math.cos(r - theta),
         }));
 
     points = points
